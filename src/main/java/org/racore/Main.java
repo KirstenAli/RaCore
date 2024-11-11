54f501566c022ea8ae4fa126b3977c994c113e3c
kirsten <ikirstenhd@gmail.com>
2024-11-11 20:23:53 +0000

    example updated

--- src/main/java/org/racore/Main.java
+++ src/main/java/org/racore/Main.java
@@ -2,31 +2,34 @@ package org.racore;
 
 import com.sun.net.httpserver.HttpExchange;
 import java.time.LocalDateTime;
+import static org.racore.Endpoint.*;
 
 public class Main {
     public static void main(String[] args) {
-        Endpoint.get("/getPerson/{id}", _ -> new Person("Alice", 30));
+        get("/getPerson/{id}", _ -> new Person("Alice", 30));
 
-        Endpoint.post("/addPerson", request -> {
+        post("/addPerson", request -> {
             Person person = request.getBodyAs(Person.class);
             return "Received Person: " + person;
         });
 
-        Endpoint.put("/updatePerson", request -> {
+        put("/updatePerson", request -> {
             Person person = request.getBodyAs(Person.class);
             return "Updated Person: " + person;
         });
 
-        Endpoint.patch("/patchPerson", request -> {
+        patch("/patchPerson", request -> {
             Person person = request.getBodyAs(Person.class);
             return "Partially updated Person: " + person;
         });
 
-        Endpoint.delete("/deletePerson", _ -> "Person deleted");
+        delete("/deletePerson", _ -> "Person deleted");
 
-        Endpoint.serveStatic("/index.html");
+        serveStatic();
 
-        Endpoint.addInterceptor(new LoggingInterceptor());
+        get("/getFile/info.txt", _ -> resolvePath("/info.txt"));
+
+        addInterceptor(new LoggingInterceptor());
     }
 
     public static class Person {
