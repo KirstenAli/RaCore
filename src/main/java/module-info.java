654c09a9d38ece9bf9152deaf0100d3c28a46b31
Kirsten <ikirstenhd@gmail.com>
2025-09-21 16:26:38 +0100

    junit test added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module org.racore {
     requires jdk.httpserver;
+    requires java.net.http;
     requires com.fasterxml.jackson.databind;
     requires org.apache.httpcomponents.core5.httpcore5;
     requires org.apache.commons.fileupload2.jakarta.servlet6;
